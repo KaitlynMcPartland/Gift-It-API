c1ebdf479ff895480661b5cd367e1bd3e3a1146e
KaitlynMcPartland <kmcp921@gmail.com>
2016-10-22 11:24:06 -0700

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show, :create] do
+    resources :gifts
+  end
+
+  get 'totals', to: 'gifts#totals'
 end
