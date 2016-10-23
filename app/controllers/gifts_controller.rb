d0efec7afcdc7e34369639ba98a75ad3fb8eb757
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2016-10-23 02:36:33 -0700

    Add icon field to params on gift post route

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -6,17 +6,19 @@ class GiftsController < ApplicationController
   end
 
   def create
+    @user = User.find(params[:user_id])
     @gift = Gift.new(
-    user_id: params[:user_id],
-    amount: params[:amount].to_i,
-    charity: params[:charity],
-    team: params[:team]
+      user_id: params[:user_id],
+      amount: params[:amount].to_i,
+      charity: params[:charity],
+      team: params[:team],
+      icon: params[:icon]
     )
 
     if @gift.save
-      render json: @gift, status: :created, location: nil
+      render json: @user
     else
-      render json: @gift.errors, status: :unprocessable_entity
+      render json: @user
     end
   end
 
